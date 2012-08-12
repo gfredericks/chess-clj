cf99404828b0f23328c0947dc6774dd958d1e7f3
Gary Fredericks <fredericksgary@gmail.com>
2012-08-12 19:49:46 -0400

    Upgrade libs; test fails

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject chess "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.macro "0.1.1"]
-                 [org.clojure/core.logic "0.7.2"]
+                 [org.clojure/core.logic "0.8-alpha2"]
                  [useful "0.8.0"]])
