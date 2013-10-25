fb30f36a0ee3e5dddd642caf58fcfe70023e891f
Gary Fredericks <fredericksgary@gmail.com>
2013-10-25 18:20:08 -0500

    Seralizer stuff for positions as FEN

--- src/com/gfredericks/chess/position.clj
+++ src/com/gfredericks/chess/position.clj
@@ -6,24 +6,62 @@
   [coll]
   (vec (map vec coll)))
 
-(defn FEN->pos
+
+(defn algebraic-square->pair
+  [alg-square]
+  (->> alg-square name seq ((fn [[a b]] [(dec (read-string (str b)))
+                                         ({\a 0 \b 1 \c 2 \d 3
+                                           \e 4 \f 5 \g 6 \h 7}
+                                          a)]))))
+
+(defn pair->algebraic-square
+  "Returns a string"
+  [[row col]]
+  (str (nth "abcdefgh" col)
+       (inc row)))
+
+(defn read-fen
   [fen]
   (let [[board turn castling ep half full] (re-seq #"\S+" fen),
         castling (set castling)
         ep (seq ep)]
-    {:board (board/read-fen-board board)
-     :turn ({"w" :white "b" :black} turn)
-     :castling {:white {:king (contains? castling \K),
-                        :queen (contains? castling \Q)}
-                :black {:king (contains? castling \k)
-                        :queen (contains? castling \q)}}
-     :en-passant (when-not (= [\-] ep)
-                   (let [[a b] ep]
-                     [(keyword (str a)) (read-string (str b))]))
-     :half-move (read-string half)
-     :full-move (read-string full)}))
+    (with-meta
+      {:board (board/read-fen-board board)
+       :turn ({"w" :white "b" :black} turn)
+       :castling {:white {:king (contains? castling \K),
+                          :queen (contains? castling \Q)}
+                  :black {:king (contains? castling \k)
+                          :queen (contains? castling \q)}}
+       :en-passant (when-not (= [\-] ep)
+                     (algebraic-square->pair ep))
+       :half-move (read-string half)
+       :full-move (read-string full)}
+      {:type ::position})))
 
-(def starting-pos (FEN->pos "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"))
+(defn ->fen
+  [{:keys [board turn castling en-passant half-move full-move]}]
+  (clojure.string/join " "
+                       [(board/board->fen-board board)
+                        ({:white "w", :black "b"} turn)
+                        (let [s (->> [[[:white :king] \K]
+                                      [[:white :queen] \Q]
+                                      [[:black :king] \k]
+                                      [[:black :queen] \q]]
+                                     (keep #(if (get-in castling (first %)) (second %)))
+                                     (apply str))]
+                          (if (empty? s) "-" s))
+                        (if en-passant
+                          (pair->algebraic-square en-passant)
+                          "-")
+                        half-move
+                        full-move]))
+
+(defmethod print-method ::position
+  [pos ^java.io.Writer w]
+  (.write w "#chess/fen ")
+  (print-method (->fen pos) w))
+
+(def starting-pos (read-fen "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"))
 
 (defn get-piece
   [pos sq]
--- src/data_readers.clj
+++ src/data_readers.clj
@@ -1,1 +1,2 @@
-{chess/fen-board com.gfredericks.chess.board/read-fen-board}
+{chess/fen-board com.gfredericks.chess.board/read-fen-board
+ chess/fen com.gfredericks.chess.position/read-fen}
