8739683005462b5f688f849291d158f4377faa5c
Gary Fredericks <fredericksgary@gmail.com>
2013-10-25 21:26:45 -0500

    Finished rules. Haha no way this works didn't even try it.

--- src/com/gfredericks/chess/rules.clj
+++ src/com/gfredericks/chess/rules.clj
@@ -125,14 +125,116 @@
         mv (normal-moves-for-piece board sq)]
     mv))
 
+(defn attacks?
+  "Returns true if the given color is attacking the given square."
+  [board attacking-color square]
+  (->> (normal-moves board attacking-color)
+       (some (fn [[from-square to-square]]
+               (and (= to-square square)
+                    ;; not a forward pawn move
+                    (not (and (= :pawn (board/piece-type (board/get board from-square)))
+                              (= (second from-square) (second to-square)))))))))
+
 (defn castling-moves
-  [board turn {:keys [king queen]}])
+  [board turn {:keys [king queen]}]
+  (let [castling-row (case turn :white 0 :black 7)
+        king-square [castling-row 4]
+        queen-rook-square [castling-row 0]
+        king-rook-square [castling-row 7]
+        attack-free? #(not (attacks? board (other-color turn) %))]
+    (if (attack-free? king-square)
+      (remove nil?
+              [(and queen
+                    (attack-free? [castling-row 3])
+                    [king-square [castling-row 2]])
+               (and king
+                    (attack-free? [castling-row 5])
+                    [king-square [castling-row 6]])]))))
 
 (defn en-passant-moves
-  [board turn en-passant-square])
+  [board turn en-passant-square]
+  (if-let [[row col] en-passant-square]
+    (let [pawn-dir (case turn :white 1 :black -1)
+          [row col] en-passant-square
+          left-sq [(- row pawn-dir) (dec col)]
+          [left-type left-color] (board/piece-info (board/get board left-sq))
+          right-sq [(- row pawn-dir) (inc col)]
+          [right-type right-color] (board/piece-info (board/get board right-sq))]
+      (remove nil?
+              [(and (square? left-sq)
+                    (= :pawn left-type)
+                    (= turn left-color)
+                    [left-sq en-passant-square])
+               (and (square? right-sq)
+                    (= :pawn right-type)
+                    (= turn right-color)
+                    [right-sq en-passant-square])]))))
+
+(defn make-move
+  "Woah man is this gonna be a workhorse."
+  [{:keys [board turn en-passant castling] :as pos} [from-square to-square promotion]]
+  (let [board' (-> board
+                   (board/set from-square :_)
+                   (board/set to-square (or promotion (board/get board from-square))))
+        piece-moved (board/piece-type (board/get board from-square))
+        [frow fcol] from-square
+        [trow tcol] to-square]
+    (-> pos
+        (assoc :board board'
+               ;; set en-passant on pawn jump
+               :en-passant (if (and (= :pawn piece-moved)
+                                    (#{2 -2} (- frow trow)))
+                             [(/ (+ frow trow) 2) fcol])
+               :turn (other-color turn))
+        (update-in [:half-move] inc)
+        (cond-> (= turn :black)
+                (update-in [:full-move] inc)
+
+                ;; move rook on castling moves
+                (and (= :king piece-moved)
+                     (#{2 -2} (- fcol tcol)))
+                (update-in [:board]
+                           (fn [board']
+                             (let [left? (= 2 tcol)
+                                   old-rook-square
+                                   [frow (if left? 0 7)]
+                                   new-rook-square
+                                   [frow (if left? 3 5)]]
+                               (-> board'
+                                   (board/set old-rook-square :_)
+                                   (board/set new-rook-square
+                                              (board/get board' old-rook-square))))))
+
+                ;; capture on en-passant moves
+                (and (= en-passant to-square)
+                     (= :pawn piece-moved))
+                (update-in [:board]
+                           (fn [board']
+                             (let [pawn-dir (case turn :white 1 :black -1)
+                                   capture-square [(- trow pawn-dir) tcol]]
+                               (board/set board' capture-square :_))))
+
+                ;; set castling on king/rook moves
+                (= :king piece-moved)
+                (assoc-in [:castling turn] {:king false :queen false})
+
+                (= :rook piece-moved)
+                (cond->
+                 (= from-square [(case turn :white 0 :black 7) 0])
+                 (assoc-in [:castling turn :queen] false)
+                 (= from-square [(case turn :white 0 :black 7) 7])
+                 (assoc-in [:castling turn :king] false))))))
 
 (defn self-checking-move?
-  [board turn move])
+  "Returns true if the move puts the moving player in check (and is
+  thus illegal)."
+  [pos move]
+  (let [board (-> pos (make-move move) (:board))
+        king-square (->> board/all-squares
+                         (filter #(= [:king (:turn pos)]
+                                     (board/piece-info (board/get board %))))
+                         (first))]
+    (attacks? board (other-color (:turn pos)) king-square)))
 
 (defn moves
   "Returns a list of all the legal moves from this position, ignoring
@@ -141,4 +243,4 @@
   (->> (concat (normal-moves board turn)
                (castling-moves board turn (castling turn))
                (en-passant-moves board turn en-passant))
-       (remove #(self-checking-move? board turn %))))
+       (remove #(self-checking-move? pos %))))
