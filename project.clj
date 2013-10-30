6e27fa1e29596773d38379e0417ecc01987b1254
Gary Fredericks <fredericksgary@gmail.com>
2013-10-29 20:46:19 -0500

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/chess "0.0.2"
+(defproject com.gfredericks/chess "0.0.3"
   :description "Okay I am writing this chess thing."
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[criterium "0.4.2"]
