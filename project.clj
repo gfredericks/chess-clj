c0258687b72d6ca3aa28683f4f2d2e76c6045147
Gary Fredericks <fredericksgary@gmail.com>
2013-11-05 10:54:46 -0600

    Optimized jvm settings for benchmarking

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Okay I am writing this chess thing."
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[criterium "0.4.2"]
-                                  [reiddraper/simple-check "0.5.1"]]}})
+                                  [reiddraper/simple-check "0.5.1"]]
+                   :jvm-opts ^:replace []}})
